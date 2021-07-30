753b50a143d448440f92a0c5a5206a22806cf3de
Alessandro <ale.proto00@gmail.com>
2021-07-30 13:36:58 +0200

    Removed json dependency, added help and upgrade

--- cget.lua
+++ cget.lua
@@ -2,35 +2,36 @@
   CraftGet by Ale32bit
   
   MIT License https://github.com/Ale32bit-CC/CraftGet/blob/master/LICENSE
-]]--
+]] --
 
 local cget = {
   _VERSION = "0.0.6",
   _AUTHORS = {
-    "Ale32bit",
+    "AlexDevs"
   },
-
   BASE_DIR = ".craftget",
   cache = {},
   sources = {},
   packages = {},
-  
-  binDir = "bin",
+  binDir = "bin"
 }
 
-local SOURCES = [[
+local SOURCES =
+  [[
 # CraftGet sources list
 # Each line is a source URL
 # Comment with #
 https://raw.githubusercontent.com/Ale32bit-CC/CraftGet/master/index.json
 ]]
 
-local JSON_URL = "https://raw.githubusercontent.com/Ale32bit-CC/CraftGet/master/json.lua"
+local json = {
+  encode = textutils.serialiseJSON,
+  decode = textutils.unserialiseJSON
+}
 
 cget.cachePath = fs.combine(cget.BASE_DIR, "cache")
 cget.packagesPath = fs.combine(cget.BASE_DIR, "packages")
 cget.sourcesPath = fs.combine(cget.BASE_DIR, "sources")
-
 
 local function fetch(url)
   local h, err = http.get(url)
@@ -44,11 +45,11 @@ end
 
 local function wget(url, path)
   local content, err = fetch(url)
-  
+
   if not content then
     return false, err
   end
-  
+
   local f = fs.open(path, "w")
   f.write(content)
   f.close()
@@ -79,11 +80,7 @@ if not fs.exists(cget.sourcesPath) then
 end
 
 if not fs.exists(fs.combine(cget.BASE_DIR, "bin")) then
-    fs.makeDir(fs.combine(cget.BASE_DIR, "bin"))
-end
-
-if not fs.exists(fs.combine(cget.BASE_DIR, "json.lua")) then
-  wget(JSON_URL, fs.combine(cget.BASE_DIR, "json.lua"))
+  fs.makeDir(fs.combine(cget.BASE_DIR, "bin"))
 end
 
 local f = fs.open(cget.cachePath, "r")
@@ -95,16 +92,14 @@ cget.packages = textutils.unserialize(f.readAll())
 f.close()
 
 for line in io.lines(cget.sourcesPath) do
-  if line:sub(1,1) ~= "#" then
+  if line:sub(1, 1) ~= "#" then
     table.insert(cget.sources, line)
   end
 end
 
 if not fs.exists(cget.binDir) then
-    fs.makeDir(cget.binDir)
-  end
-
-local json = dofile(fs.combine(cget.BASE_DIR, "json.lua"))
+  fs.makeDir(cget.binDir)
+end
 
 local function getPackage(address)
   for k, v in ipairs(cget.cache) do
@@ -115,9 +110,8 @@ local function getPackage(address)
 end
 
 local function installPackage(address, force) -- install packages, recursive stuff
-
   local pkg = getPackage(address)
-  
+
   if not force and cget.packages[address] then
     if cget.packages[address].version == pkg.version then
       return true
@@ -125,14 +119,12 @@ local function installPackage(address, force) -- install packages, recursive stu
   end
 
   print("Installing " .. address)
-  
+
   if not pkg then
-    printError("Package ".. address .." not found")
+    printError("Package " .. address .. " not found")
     return false
   end
-  
-  
-  
+
   if type(pkg.files) == "table" then
     for path, url in pairs(pkg.files) do
       local ok, err = wget(url, path)
@@ -141,9 +133,9 @@ local function installPackage(address, force) -- install packages, recursive stu
       end
     end
   end
-  
+
   cget.packages[address] = pkg
-  
+
   if type(pkg.dependencies) == "table" then
     for _, dAddr in ipairs(pkg.dependencies) do
       local dep = getPackage(dAddr)
@@ -154,17 +146,17 @@ local function installPackage(address, force) -- install packages, recursive stu
       end
     end
   end
-  
+
   if type(pkg.autorun) == "table" then
     for _, cmd in ipairs(pkg.autorun) do
       if shell then
         shell.run(cmd)
       else
-        printError("Cannot execute \"" .. cmd .. "\": Shell not found")
+        printError('Cannot execute "' .. cmd .. '": Shell not found')
       end
     end
   end
-  
+
   local f = fs.open(cget.packagesPath, "w")
   f.write(textutils.serialize(cget.packages))
   f.close()
@@ -174,10 +166,10 @@ end
 local function sync()
   print("Syncing with sources...")
   local final = {}
-  
+
   for i, url in ipairs(cget.sources) do
     print(i .. ": " .. url)
-   
+
     local source, err = fetch(url)
     if source then
       source = json.decode(source)
@@ -189,29 +181,29 @@ local function sync()
             pkg.meta = {
               name = name,
               address = maintainer .. "/" .. name,
-              maintainer = maintainer,
+              maintainer = maintainer
             }
             final[maintainer .. "/" .. name] = pkg -- Avoid duplicates
           end
         else
-          printError("Cannot fetch " .. indexURL ..": ".. err)
+          printError("Cannot fetch " .. indexURL .. ": " .. err)
         end
       end
     else
       printError("Cannot fetch: " .. err)
     end
   end
-  
+
   cget.cache = {}
-  
+
   for address, pkg in pairs(final) do
     table.insert(cget.cache, pkg)
   end
-  
+
   local f = fs.open(cget.cachePath, "w")
   f.write(textutils.serialize(cget.cache))
   f.close()
-  
+
   print("Updated " .. #cget.cache .. " entries")
 end
 
@@ -222,7 +214,7 @@ local function install(...)
     sync()
     local newArgs = {}
     for i = 2, #args do
-        table.insert(newArgs, args[i])
+      table.insert(newArgs, args[i])
     end
     args = newArgs
   end
@@ -233,10 +225,10 @@ local function install(...)
   end
   local forceYes = requestedPackages["-y"] or false
   requestedPackages["-y"] = nil
-  
+
   local packages = {}
   local willInstall = {}
-  
+
   for name in pairs(requestedPackages) do
     local possibilities = {}
     for _, pkg in ipairs(cget.cache) do
@@ -247,14 +239,14 @@ local function install(...)
         table.insert(possibilities, pkg)
       end
     end
-    
-    local pkg 
-    
+
+    local pkg
+
     if #possibilities > 1 then
       print("There are multiple packages with the same name.")
       print("Pick the package you need:")
       for i = 1, #possibilities do
-        print("- " .. i ..": " .. possibilities[i].meta.address)
+        print("- " .. i .. ": " .. possibilities[i].meta.address)
       end
       while true do
         write("> ")
@@ -266,7 +258,6 @@ local function install(...)
           print("Picked " .. possibilities[input].meta.address)
           break
         end
-        
       end
     elseif #possibilities == 1 then
       pkg = possibilities[1]
@@ -274,18 +265,18 @@ local function install(...)
       printError("Package " .. name .. " not found!")
       pkg = nil
     end
-    
+
     if pkg then
       table.insert(packages, pkg.meta.address)
       table.insert(willInstall, pkg.meta.name .. "@" .. pkg.version or "1.0.0")
     end
   end
-  
+
   if #packages == 0 then
     print("No packages found")
     return
   end
-  
+
   if not forceYes then
     print("The following packages will be installed: ")
     print(" - " .. table.concat(willInstall, ", "))
@@ -294,12 +285,24 @@ local function install(...)
       local _, key = os.pullEvent("key")
     until key == keys.enter
   end
-  
+
   for _, pkg in ipairs(packages) do
     installPackage(pkg, true)
   end
-  
+
   print("Installed " .. #packages .. " packages")
+end
+
+local function upgrade(...)
+  if select("#", ...) == 0 then
+    local packages = {}
+    for k in pairs(cget.packages) do
+      table.insert(packages, k)
+    end
+    install(unpack(packages))
+  else
+    install(...)
+  end
 end
 
 local function remove(...)
@@ -310,9 +313,9 @@ local function remove(...)
   end
   local forceYes = requestedPackages["-y"] or false
   requestedPackages["-y"] = nil
-  
+
   local packages = {}
-  
+
   for name in pairs(requestedPackages) do
     local possibilities = {}
     for _, pkg in ipairs(cget.cache) do
@@ -323,14 +326,14 @@ local function remove(...)
         table.insert(possibilities, pkg)
       end
     end
-    
-    local pkg 
-    
+
+    local pkg
+
     if #possibilities > 1 then
       print("There are multiple packages with the same name.")
       print("Pick the package you need to remove:")
       for i = 1, #possibilities do
-        print("- " .. i ..": " .. possibilities[i].meta.address)
+        print("- " .. i .. ": " .. possibilities[i].meta.address)
       end
       while true do
         write("> ")
@@ -342,7 +345,6 @@ local function remove(...)
           print("Picked " .. possibilities[input].meta.address)
           break
         end
-        
       end
     elseif #possibilities == 1 then
       pkg = possibilities[1]
@@ -350,17 +352,17 @@ local function remove(...)
       printError("Package " .. name .. " not found!")
       pkg = nil
     end
-    
+
     if pkg then
       table.insert(packages, pkg.meta.address)
     end
   end
-  
+
   if #packages == 0 then
     print("No packages found")
     return
   end
-  
+
   if not forceYes then
     print("The following packages will be removed: ")
     print(" - " .. table.concat(packages, ", "))
@@ -369,10 +371,10 @@ local function remove(...)
       local _, key = os.pullEvent("key")
     until key == keys.enter
   end
-  
+
   for _, addr in ipairs(packages) do
     local pkg = getPackage(addr)
-    
+
     if pkg then
       if type(pkg.files) == "table" then
         for path in pairs(pkg.files) do
@@ -381,18 +383,20 @@ local function remove(...)
       end
     end
   end
-  
+
   print("Removed " .. #packages .. " packages")
 end
 
 local function list()
   local list = {}
-  
+
   for i, pkg in ipairs(cget.cache) do
     table.insert(list, string.format("%d. %s @ %s", i, pkg.meta.address, pkg.version))
   end
-  
-  textutils.pagedTabulate(list)
+
+  local _, h = term.getSize()
+
+  textutils.pagedPrint(table.concat(list, "\n"), h)
 end
 
 local function editSources()
@@ -400,40 +404,47 @@ local function editSources()
 end
 
 local function init()
-    shell.setPath(shell.path() .. ":/bin:/.craftget/bin")
-    shell.setAlias("craftget", "cget")
+  shell.setPath(shell.path() .. ":/bin:/.craftget/bin")
+  shell.setAlias("craftget", "cget")
 end
 
-local args = {...}
-
-local commands = {
-  {"sync", sync, "- Update packages list from sources"},
-  {"install", install, "<package>... - Install packages"},
-  {"remove", remove, "<package>... - Remove packages"},
-  {"list", list, "- List packages in cache"},
-  {"edit", editSources, "- Edit sources file"},
-  {"init", init, "- Integrate with shell"},
-}
-
-print("CraftGet " .. cget._VERSION)
-
-if #args < 1 then
+local commands
+local function help()
   print("Usage: cget <command> [args[, ...]]")
   print("Available commands:")
   for i, cmd in ipairs(commands) do
     print("- " .. cmd[1] .. " " .. (cmd[3] or "[No usage specified]"))
   end
+end
+
+local args = {...}
+
+commands = {
+  {"help", help, "- Show this help message"},
+  {"update", sync, "- Update packages list from sources"},
+  {"install", install, "<package>... - Install packages"},
+  {"upgrade", upgrade, "[package, ...] - Upgrade packages to latest version"},
+  {"remove", remove, "<package>... - Remove packages"},
+  {"list", list, "- List packages in cache"},
+  {"edit", editSources, "- Edit sources file"},
+  {"init", init, "- Integrate with shell"}
+}
+
+print("CraftGet " .. cget._VERSION)
+
+if #args < 1 then
+  help()
   return
 end
 
 for i, cmd in ipairs(commands) do
-    if cmd[1] == args[1] then
-      local cmdArgs = {}
-      for i = 2, #args do
-        table.insert(cmdArgs, args[i])
-      end
-      cmd[2](unpack(cmdArgs))
-      return
+  if cmd[1] == args[1] then
+    local cmdArgs = {}
+    for i = 2, #args do
+      table.insert(cmdArgs, args[i])
     end
+    cmd[2](unpack(cmdArgs))
+    return
+  end
 end
 print("Command not found")
